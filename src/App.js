ab57f69a4dd9c332017d9279ecf5fb94e27dca76
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-01-22 15:25:07 -0800

    bare bones about and home pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
 import './App.css';
+import {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Home from './Components/Pages/Home'
+import About from './Components/Pages/About'
+import Navbar from './Components/Layout/Navbar'
 
 const App = ()  => {
   return (
-    <div className="App">
-      My app
-    </div>
+    <Router>
+    <Fragment className="App">
+     <Navbar/>
+     <div className="container">
+       <Switch>
+         <Route exact path='/' component={Home}/>
+         <Route exact path='/about' component={About}/>
+       </Switch>
+     </div>
+    </Fragment>
+    </Router>
   );
 }
 
