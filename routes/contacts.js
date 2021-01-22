03aca203fe6b3be6808ea73810c5e49bc8c8f621
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-01-22 13:54:02 -0800

    added edit functionality

--- routes/contacts.js
+++ routes/contacts.js
@@ -9,7 +9,6 @@ const Contact = require('../models/Contact');
 //@desc Get all users contacts
 //@access Private
 router.get('/', auth, async (req,res) => {
-  
   try {
     const contacts = await Contact.find({user: req.user.id}).sort({date: -1});
     res.json(contacts)
@@ -48,9 +47,38 @@ router.post('/', [auth,  [
 //@route PUT api/contacts/:id
 //@desc Edit a contact
 //@access Private
-router.put('/', (req,res) => {
-  res.send('edit a contact')
-})
+router.put('/:id',auth, async (req,res) => {
+  const {name, email, phone, type} = req.body;
+
+  //build a contact object
+  const contactFields = {};
+  if(name) contactFields.name = name
+  if(email) contactFields.email = email
+  if(phone) contactFields.phone = phone
+  if(type) contactFields.type = type
+
+  try {
+    let contact = await Contact.findById(req.params.id)
+
+    if(!contact) return res.status(404).json({msg: 'Contact not found'})
+
+    //make sure user owns contact
+    if(contact.user.toString() !== req.user.id){
+      return res.status(401).json({msg: 'Not authorized'})
+    }
+
+    contact = await Contact.findByIdAndUpdate(req.params.id,
+      { $set: contactFields},
+      {new:true})
+
+      res.json(contact)
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('server error')
+  }
+});
+
+
 //@route DELETE api/contacts/:id
 //@desc Delete a contact
 //@access Private
