bf31400ecc48a2eeb877d4384c9ed00f2495d2fb
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2021-01-22 16:08:41 -0800

    contacts pulled from state

---
+++ src/Components/Contacts/Contacts.js
@@ -0,0 +1,19 @@
+import React, {useContext, Fragment} from 'react'
+import ContactContext from '../../Context/Contact/ContactContext';
+
+
+const Contacts = () => {
+
+  const contactContext = useContext(ContactContext);
+
+  const {contacts} = contactContext;
+  return (
+    <Fragment>
+      {contacts.map(contact => 
+      <h3>{contact.name}</h3>
+      )}
+    </Fragment>
+  )
+}
+
+export default Contacts
